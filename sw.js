de2bf296158723f0a0111ff398bd59a04dc86585
filipsslavov <fslavo2@gmail.com>
2026-08-24 14:52:40 -0500

    Add PNG icons for Android packaging

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'os-shell-v1';
-const SHELL = ['./', './index.html', './manifest.json', './icon.svg'];
+const CACHE = 'os-shell-v2';
+const SHELL = ['./', './index.html', './manifest.json', './icon.svg', './icon-192.png', './icon-512.png', './icon-512-maskable.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)));
